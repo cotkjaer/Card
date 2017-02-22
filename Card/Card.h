ea19920fbd2262a41e5ab8c9d598c61d8d7d42d9
christian otkjær <christian.otkjaer@gmail.com>
2017-02-22 07:46:06 +0100

    Initial Commit

---
+++ Card/Card.h
@@ -0,0 +1,19 @@
+//
+//  Card.h
+//  Card
+//
+//  Created by Christian Otkjær on 22/02/17.
+//  Copyright © 2017 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Card.
+FOUNDATION_EXPORT double CardVersionNumber;
+
+//! Project version string for Card.
+FOUNDATION_EXPORT const unsigned char CardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Card/PublicHeader.h>
+
+
